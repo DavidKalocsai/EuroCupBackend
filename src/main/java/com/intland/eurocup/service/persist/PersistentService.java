bf295f6d15dc5b245ea3d3987cb79370a8309cc4
David Kalocsai <kalocsaidavid@gmail.com>
2019-04-15 08:26:47 +0200

    PersistentService interface intruduced

--- src/main/java/com/intland/eurocup/service/persist/PersistentService.java
+++ src/main/java/com/intland/eurocup/service/persist/PersistentService.java
@@ -1,37 +1,24 @@
 package com.intland.eurocup.service.persist;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.intland.eurocup.model.Voucher;
-import com.intland.eurocup.repository.VoucherRepository;
 
-@Service
-public class PersistentService {	
+/**
+ * Interface to persist {@link Voucher}. 
+ */
+public interface PersistentService {
+	/**
+	 * Save or update {@link Voucher}. 
+	 * @param voucher {@link Voucher}
+	 * @return persisted {@link Voucher}
+	 */
+	Voucher save(Voucher voucher);
 	
-	@Autowired
-	private VoucherRepository repository;
+	/**
+	 * Save voucher only if not yet in DB and retrieve it back to have to full data. 
+	 * @param voucher {@link Voucher} - converted from incoming data
+	 * @return {@link Voucher} - retrieved from DB
+	 */
+	Voucher saveIfAbsent(Voucher voucher); 
 	
-	public Voucher save(final Voucher voucher) {
-		Voucher retVoucher = retrieveVoucherFromDBIfInDB(voucher); 
-		return saveVoucherIfNotInDB(voucher, retVoucher);
-	}
 	
-	private Voucher retrieveVoucherFromDBIfInDB(final Voucher voucher) {
-		final List<Voucher> vouchers =  repository.findByEmailAndVoucherAndTerritory(voucher.getEmail(), voucher.getVoucher(), voucher.getTerritory().toString());
-		return vouchers.isEmpty() ? null : vouchers.get(0);
-	}
-	
-	private Voucher saveVoucherIfNotInDB(final Voucher voucher, Voucher retVoucher) {
-		if (retVoucher == null) {
-			retVoucher = repository.save(voucher);
-		}
-		return retVoucher;
-	}
-	
-	public Voucher update(final Voucher voucher) {
-		return repository.save(voucher);
-	}
 }
